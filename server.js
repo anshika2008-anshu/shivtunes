ee135d2b58ef62c50606bed1b00c4effe9498251
anshika2008-anshu <anshikashivhare835@gmail.com>
2026-05-30 10:57:12 +0530

    fix login and server update

--- server.js
+++ server.js
@@ -93,12 +93,12 @@ app.post("/login", async (req, res) => {
 ============================*/
 
 
-app.use(express.static(__dirname));
+mongoose.connect("mongodb+srv://shivuser:Shiv%4012345@cluster0.jhxqz1t.mongodb.net/musicApp?retryWrites=true&w=majority")
+.then(() => {
+    console.log("MongoDB Connected");
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
-});
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Server running");
+    });
+})
+.catch(err => console.log(err));
